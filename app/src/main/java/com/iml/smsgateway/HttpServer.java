08586f76b03184c637d79b11e8644971513957ba
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2020-01-09 09:56:11 +0800

    First commit

---
+++ app/src/main/java/com/iml/smsgateway/HttpServer.java
@@ -0,0 +1,165 @@
+package com.iml.smsgateway;
+import android.content.*;
+import java.net.*;
+import java.io.*;
+import android.app.*;
+import java.util.*;
+import android.telephony.gsm.*;
+
+public class HttpServer implements Runnable
+{
+	
+	public final static int STATUS_RUNNING = 0;
+	public final static int STATUS_STOP = 1;
+	public final static int STATUS_STARTING = 2;
+	
+	public final static int RESPONSE_INVALID_PARAMETERS = 1;
+	public final static int RESPONSE_SUCCESS = 2;
+	
+	public final static int PORT = 8080;
+	
+	
+	
+	private static int status = 1;
+	public static MainActivity activity;
+	private static ServerSocket server;
+	private Socket client;
+	private OutputStream os;
+	private InputStream is;
+	
+	public HttpServer(Socket socket)
+	{
+		client = socket;
+		try
+		{
+			os = socket.getOutputStream();
+			is = socket.getInputStream();
+		}
+		catch (IOException e)
+		{
+			System.err.println(e);
+		}
+	}
+	
+	
+	public static int getStatus()
+	{
+		return status;
+	}
+	
+	
+	public static void stopServer()
+	{
+		activity.addLogs("Stopping server");
+		status = STATUS_STOP;
+		try
+		{
+			server.close();
+		}
+		catch (IOException e)
+		{
+			System.err.println(e);
+		}
+	}
+	
+	public static void startServer(final MainActivity app)
+	{
+		
+		
+		Thread t = new Thread(new Runnable(){
+
+				@Override
+				public void run()
+				{
+					// TODO: Implement this method
+					HttpServer.activity = app;
+					app.addLogs("Starting Server");
+					HttpServer.status = STATUS_STARTING;
+					try {
+						HttpServer.server = new ServerSocket(PORT);
+						HttpServer.status = STATUS_RUNNING;
+						app.addLogs("Server running on port "+PORT);
+						app.stopButton();
+						while(true){
+							if (HttpServer.status == STATUS_RUNNING){
+								Thread request = new Thread(new HttpServer(server.accept()));
+								request.start();
+							} else {break;}
+						}
+					}catch(IOException ioe){
+						if (HttpServer.status == STATUS_STOP) return;
+						HttpServer.status = STATUS_STOP;
+						app.addLogs("Can't start server on port "+PORT);
+						app.startButton();
+						System.err.println(ioe);
+					}
+				}
+			});
+		t.start();
+	}
+	
+	@Override
+	public void run()
+	{
+		// TODO: Implement this method
+		activity.addLogs("Incoming Request");
+		try
+		{
+			Map<String, String> query = getQuery();
+			if(query.containsKey("number") && query.containsKey("message")){
+				activity.addLogs("Sending message to "+query.get("number"));
+				sendMessage(query);
+			} else {
+				activity.addLogs("Invalid Parameters");
+				writeResponse("{\"status\":"+RESPONSE_INVALID_PARAMETERS+",\"message\":\"Failed: Invalid parameters\"}");
+			}
+		}
+		catch (IOException e)
+		{
+			System.err.println(e);
+		} finally {
+			try
+			{
+				client.close();
+			}
+			catch (IOException e)
+			{
+				System.err.println(e);
+			}
+		}
+	}
+	
+	private void sendMessage(Map<String,String> query){
+		SmsManager sms = SmsManager.getDefault();
+		sms.sendTextMessage(query.get("number"), null, query.get("message"), null, null);
+		writeResponse("{\"status\":"+RESPONSE_SUCCESS+",\"message\":\"Success: Message sent successfully\"}");
+		activity.addLogs("Message sent");
+	}
+	
+	private void writeResponse(String body){
+		PrintWriter pw = new PrintWriter(os);
+		pw.println("HTTP/1.0 200 OK");
+		pw.println("Content-Length: "+body.length());
+		pw.println("");
+		pw.print(body);
+		pw.flush();
+	}
+	
+	private Map<String,String> getQuery() throws IOException{
+		Map<String, String> res= new HashMap<String, String>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		String[] query = br.readLine().split(" ")[1].split("\\?");
+		if(query.length > 1){
+			String[] params = query[1].split("\\&");
+			for(int i = 0; i < params.length; i++){
+				String key = params[i].split("\\=")[0];
+				String value = "";
+				if (params[i].split("\\=").length > 1){
+					value = params[i].split("\\=")[1];
+				}
+				res.put(key, value);
+			}
+		}
+		return res;
+	}
+}
---
+++ app/src/main/java/com/iml/smsgateway/MainActivity.java
@@ -0,0 +1,106 @@
+package com.iml.smsgateway;
+
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+import java.util.*;
+import java.text.*;
+import android.view.View.*;
+import android.view.*;
+import android.content.*;
+import android.graphics.*;
+
+public class MainActivity extends Activity 
+{
+	public Button start;
+	public TextView status;
+	private static String logs = "App Started";
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		
+		addLogs(logs);
+		
+		start = (Button) findViewById(R.id.start);
+		status = (TextView) findViewById(R.id.status);
+		
+		if (HttpServer.getStatus() == HttpServer.STATUS_RUNNING){
+			stopButton();
+		}
+		final MainActivity app = this;
+		start.setOnClickListener(new OnClickListener(){
+
+				@Override
+				public void onClick(View p1)
+				{
+					// TODO: Implement this method
+					if (HttpServer.getStatus() == HttpServer.STATUS_STOP){
+						HttpServer.startServer(app);
+						//stopButton();
+					} else if (HttpServer.getStatus() == HttpServer.STATUS_RUNNING){
+						HttpServer.stopServer();
+						startButton();
+					}
+				}
+			});
+    }
+	
+	public void startButton()
+	{
+		runOnUiThread(new Runnable(){
+
+				@Override
+				public void run()
+				{
+					// TODO: Implement this method
+					start.setText("Start server");
+					start.setBackgroundResource(R.drawable.green_button);
+					start.setTextColor(Color.rgb(0, 255, 0));
+					status.setText("Not running");
+					status.setTextColor(Color.rgb(255,0,0));
+				}
+			});
+	}	
+	
+	public void stopButton()
+	{
+		runOnUiThread(new Runnable(){
+
+				@Override
+				public void run()
+				{
+					start.setText("Stop server");
+					start.setBackgroundResource(R.drawable.pink_button);
+					start.setTextColor(Color.rgb(250, 113, 205));
+					status.setText("Running");
+					status.setTextColor(Color.rgb(0,255,0));
+					
+					// TODO: Implement this method
+				}
+		});
+	}
+	
+	public void addLogs(final String log1)
+	{
+		
+		runOnUiThread(new Runnable(){
+
+				@Override
+				public void run()
+				{
+					// TODO: Implement this method
+					Date now = new Date();
+					SimpleDateFormat ft = new SimpleDateFormat("hh:mm:ss");
+					String log = ft.format(now) + " " + log1 + "\n";
+					TextView logs = (TextView) findViewById(R.id.logs);
+					log += logs.getText().toString();
+					logs.setText(log);
+					MainActivity.logs = log;
+				}
+			});
+		
+		
+	}
+}
